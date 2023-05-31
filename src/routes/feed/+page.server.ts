f26cc35ee1ba98f32429b6693301b09f9b01101d
James (Jamalam) <james@jamalam.tech>
2023-05-31 22:24:05 +0100

    feat: infinite scroll

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
+const FETCH_INCREMENT = 10;
+
 export const load: PageServerLoad = async ({ locals: { getSession, supabase } }) => {
 	const session = await getSession();
 
@@ -8,29 +10,27 @@ export const load: PageServerLoad = async ({ locals: { getSession, supabase } })
 		throw redirect(303, "/login");
 	}
 
+	const { data: previousVotes } = await supabase
+		.from("post_reactions")
+		.select("post_id")
+		.eq("user_id", session.user.id);
+	
 	const { data, error } = await supabase
 		.from("posts")
 		.select("*")
 		.order("created_at", { ascending: false })
 		.eq("approved", true)
-		.limit(10);
+		.not("id", "in", `(${(previousVotes || []).map((post) => post.post_id).join(",")})`)
+		.limit(FETCH_INCREMENT);	
 
-	if (error) {
+	if (error || !data) {
 		console.error(error);
-	}
-
-	const { data: reactions, error: votesError } = await supabase
-		.from("post_reactions")
-		.select("*")
-		.eq("user_id", session.user.id);
-
-	if (votesError) {
-		console.error(votesError);
 	}
 
 	return {
 		session,
 		user: session.user,
-		posts: (data || []).filter((p) => (reactions || []).findIndex((r) => r.post_id === p.id) == -1),
+		posts: data || [],
+		fetchIncrement: FETCH_INCREMENT,
 	};
 };
--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -7,11 +7,13 @@
 	import "./cards.css";
 
 	export let data: PageData;
+	let posts = data.posts;
 
-	const interactThreshold = 160;
-	const interactMaxRotation = 15;
+	const INTERACT_THRESHOLD = 160;
+	const INTERACT_MAX_ROTATION = 15;
 
 	let currentIdx = 0;
+	let currentFetchIdx = data.fetchIncrement + 1;
 	let rotation = 0;
 	let x = 0;
 	let y = 0;
@@ -33,9 +35,9 @@
 				x = ((getCSSCustomProp("--card-x", ev.target as HTMLElement, "float") || 0) as number) + ev.dx;
 				y = ((getCSSCustomProp("--card-y", ev.target as HTMLElement, "float") || 0) as number) + ev.dy;
 
-				rotation = interactMaxRotation * (x / interactThreshold);
-				if (rotation > interactMaxRotation) rotation = interactMaxRotation;
-				else if (rotation < -interactMaxRotation) rotation = -interactMaxRotation;
+				rotation = INTERACT_MAX_ROTATION * (x / INTERACT_THRESHOLD);
+				if (rotation > INTERACT_MAX_ROTATION) rotation = INTERACT_MAX_ROTATION;
+				else if (rotation < -INTERACT_MAX_ROTATION) rotation = -INTERACT_MAX_ROTATION;
 
 				ev.target.style.setProperty("--card-x", x + "px");
 				ev.target.style.setProperty("--card-y", y + "px");
@@ -46,7 +48,7 @@
 				ev.target.setAttribute("data-dragging", "false");
 				let moved = Math.abs(ev.pageX - ev.x0);
 
-				if (moved > interactThreshold) {
+				if (moved > INTERACT_THRESHOLD) {
 					ev.target.setAttribute("data-status", "transition");
 
 					if (x > 0) {
@@ -63,8 +65,34 @@
 						ev.target.nextElementSibling.setAttribute("data-status", "current");
 					}
 
-					voteOnPost(data.supabase, data.session, data.posts[currentIdx], x > 0 ? "upvote" : "downvote");
+					voteOnPost(data.supabase, data.session, posts[currentIdx], x > 0 ? "upvote" : "downvote");
 					currentIdx++;
+
+					if (currentIdx > posts.length - 5) {
+						data.supabase
+							.from("post_reactions")
+							.select("post_id")
+							.eq("user_id", data.session.user.id)
+							.then(({ data: previousVotes }) => {
+								data.supabase
+									.from("posts")
+									.select("*")
+									.order("created_at", { ascending: false })
+									.eq("approved", true)
+									.not("id", "in", `(${(previousVotes || []).map((post) => post.post_id).join(",")})`)
+									.range(currentFetchIdx, currentFetchIdx + data.fetchIncrement)
+									.then((res) => {
+										let { data: newData, error } = res;
+
+										if (newData) {
+											posts = [...posts, ...newData];
+											currentFetchIdx += newData.length;
+										} else if (!data || error) {
+											console.error(data, error);
+										}
+									});
+							});
+					}
 				} else {
 					x = 0;
 					y = 0;
@@ -80,7 +108,7 @@
 </script>
 
 <div class="w-full">
-	{#each data.posts as post, i}
+	{#each posts as post, i}
 		{#key post.id}
 			<Post {post} supabase={data.supabase} status={i == 0 ? "current" : "waiting"} />
 		{/key}
