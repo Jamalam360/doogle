3033a28e80a98663457aa7c7919098e0a155b6a3
James (Jamalam) <james@jamalam.tech>
2023-06-01 12:37:30 +0100

    fix: card styling changes

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -6,6 +6,8 @@
 	export let post: Database["public"]["Tables"]["posts"]["Row"];
 	export let status: "current" | "done" | "waiting" = "waiting";
 
+	let profile =
+		post.user_id == null ? null : supabase.from("profiles").select("*").eq("id", post.user_id).limit(1).single();
 	let value = 0;
 
 	$: {
@@ -29,13 +31,30 @@
 				}
 			});
 	}
+
+	$: {
+	}
 </script>
 
-<div
-	class="card rounded-3xl bg-sky-100"
-	data-dragging="false"
-	data-status={status}
->
-	<h2 class="pt-2 text-center text-4xl text-[#404b55] font-bold">{value}</h2>
+<div class="card rounded-3xl bg-sky-100" data-dragging="false" data-status={status}>
+	<div class="flex items-center space-x-4">
+		{#if profile}
+			{#await profile then { data: profile }}
+				{#if profile}
+					<a href={`/profile/${profile.id}`}
+						><img src={profile.avatar} alt="avatar" class="h-10 w-10 rounded-full border border-gray-300" /></a
+					>
+					<a href={`/profile/${profile.id}`}
+						><h2 class="text-2xl text-[#404b55] font-bold">{profile.username.split("#")[0]}</h2>
+					</a>
+				{/if}
+			{/await}
+		{:else}
+			<img src="/system.jpg" alt="avatar" class="h-10 w-10 rounded-full border border-gray-300" />
+			<h2 class="text-2xl text-[#404b55] font-bold">System</h2>
+		{/if}
+		<!-- Share button here -->
+	</div>
 	<img src={post.image} alt="Dog" class="h-[60vh] w-full aspect-[9/16] object-cover p-2 rounded-3xl" />
+	<h2 class="pt-2 text-center text-4xl text-[#404b55] font-bold">{value}</h2>
 </div>
